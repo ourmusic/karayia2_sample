e4f5e67ad04cb9e38eefe536367ea12ed29233fe
hrkarayiannis <hrkarayiannis@gmail.com>
2015-02-21 11:42:17 -0600

    :)

---
+++ Puzzle/Puzzle/Puzzle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Puzzle.Puzzle
+{
+    [HubName("moveShape")]
+    public class Puzzle : Hub
+    {
+        public void moveShape(double x, double y)
+        {
+            Clients.Others.shapeMoved(x, y);
+        }
+    }
+}
